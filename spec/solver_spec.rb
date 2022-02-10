dc7f4ed81b89745c0eb1fcb9187b7808262d0553
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-10 03:12:21 -0500

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,17 @@
-require "solver"
+require 'solver'
 
 describe Solver do
   before :each do
     @solver = Solver.new
   end
 
-  context "Given a single parameter" do
+  context 'Given a single parameter' do
     it 'is an instance of Solver' do
       @solver.should be_an_instance_of Solver
     end
+  end
+
+  it 'can calculate the factorial of an integer using the factorial method' do
+    expect(@solver.factorial(5)).to eq(120)
   end
 end
